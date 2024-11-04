a49bc4f796d17042bb4a375a5d242f7b4526fac9
Gilang Indra <gilangindra734@gmail.com>
2024-11-04 10:00:03 +0700

    Menambahkan routing antar pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import logo from "../assets/logoimage.png";
-import Item from "./Item"
+import Item from "./Item";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 const Sidebar = ({ menuItems, logo }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLandingPageClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="w-64 bg-blue-900 text-white">
       <div className="p-4">
-        <div className="flex items-center space-x-2">
+        <div
+          onClick={handleLandingPageClick}
+          className="flex items-center space-x-2"
+        >
           <img src={logo} alt="SISKPTIF Logo" className="w-12 h-12" />
           <span className="text-xl font-bold">SISKPTIF</span>
         </div>
@@ -14,12 +25,16 @@ const Sidebar = ({ menuItems, logo }) => {
 
       <nav className="mt-8">
         {menuItems.map((item, index) => (
-          <Item
+          <Link
             key={index}
-            icon={item.icon}
-            text={item.text}
-            active={item.active}
-          />
+            to={item.path}
+            className={`flex items-center space-x-2 px-6 py-3 hover:bg-blue-800 transition-colors ${
+              location.pathname === item.path ? "bg-blue-800" : ""
+            }`}
+          >
+            {item.icon}
+            <span>{item.text}</span>
+          </Link>
         ))}
       </nav>
     </div>
