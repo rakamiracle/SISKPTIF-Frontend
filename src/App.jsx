ff1bd8cca2604e4300382d7ce75947a1aa063dfa
Gilang Indra <gilangindra734@gmail.com>
2024-11-04 08:28:57 +0700

    Menambahkan routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React, {useState} from "react";
-import Login from "../src/pages/Login"
-import Dashboard from "../src/pages/mahasiswa/Dashboard"
-import LandingPage from "./pages/LandingPages"
+import React, { useState } from "react";
+import Login from "../src/pages/Login";
+import Dashboard from "../src/pages/mahasiswa/Dashboard";
+import LandingPage from "./pages/LandingPages";
+import Pengajuan from "./pages/mahasiswa/Pengajuan";
+import Status from "../src/pages/mahasiswa/Status";
+import Pendaftaran from "../src/pages/mahasiswa/Pendaftaran";
+import DashboardLayout from "./components/DashboardLayout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/pengajuan" element={<Pengajuan />} />
+          <Route path="/pendaftaran" element={<Pendaftaran />} />
+          <Route path="/status" element={<Status />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
