8b9b0c0fbe45cd49ed7f87bdf5112506fc86ab93
Gilang Indra <gilangindra734@gmail.com>
2024-11-13 06:11:45 +0700

    🔥 feat: replace logo format from SVG to icon component in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Notification from "./Notification";
-import UserMenu from "./UserMenu";
+import {Menu} from "lucide-react";
 
 const Navbar = ({ title, onToggleSidebar }) => {
   return (
@@ -11,9 +11,7 @@ const Navbar = ({ title, onToggleSidebar }) => {
             className="text-gray-500 hover:text-gray-700 focus:outline-none"
             onClick={onToggleSidebar}
           >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
+            <Menu className="w-6 h-6"/>
           </button>
           <h1 className="text-xl font-medium">{title}</h1>
         </div>
