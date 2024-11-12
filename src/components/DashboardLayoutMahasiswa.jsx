653be2fbd42e4cf985a9f1f9cfa315c2d0709b0b
Gilang Indra <gilangindra734@gmail.com>
2024-11-13 05:51:52 +0700

    ⚙ refactor: improve layout structure for sidebar and main content

--- src/components/DashboardLayoutMahasiswa.jsx
+++ src/components/DashboardLayoutMahasiswa.jsx
@@ -5,31 +5,35 @@ import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [currentMenuItem, setCurrentMenuItem] = useState('Dashboard');
+  const [currentMenuItem, setCurrentMenuItem] = useState('SITRACK');
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 relative">
       <Navbar title={currentMenuItem} onToggleSidebar={toggleSidebar} />
-      <Sidebar isOpen={isSidebarOpen} />
-      
-      {/* Main Content */}
-      <div className={`transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>
-        <main className="p-8 mt-16">
-          <Outlet />
-        </main>
-      </div>
-      
-      {/* Overlay untuk mobile */}
-      {isSidebarOpen && (
-        <div
-          className="fixed inset-0 z-10 bg-gray-900/50 lg:hidden"
-          onClick={toggleSidebar}
-        />
-      )}
+        <div className="flex">
+            <Sidebar isOpen={isSidebarOpen}/>
+            <div
+                className={`transition-all duration-300 flex-1 ${
+                    isSidebarOpen ? 'lg:ml-64' : ''
+                }`}
+            >
+                <main className="p-8 mt-16">
+                    <Outlet/>
+                </main>
+            </div>
+
+            {/* Overlay untuk mobile */}
+            {isSidebarOpen && (
+                <div
+                    className="fixed inset-0 z-10 bg-gray-900/50 lg:hidden"
+                    onClick={toggleSidebar}
+                />
+            )}
+        </div>
     </div>
   );
 };
