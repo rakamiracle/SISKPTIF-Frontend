a30d998d57203483730620db19ee429a87b21340
Gilang Indra <gilangindra734@gmail.com>
2024-11-13 05:48:25 +0700

    ⚙ refactor : simplify ClickedText component props structure

--- src/components/ClickedText.jsx
+++ src/components/ClickedText.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-const ClickedText = (props) => {
-  const { color = "primary", hoverColor = "secondary", children = "Click me" } = props;
-  return (
-    <div className="text-sm">
-      <a
-        href="#"
-        className="font-instrument font-medium text-${props.color} hover:text-${props.hoverColor}"
-      >
-        {props.children}
-      </a>
-    </div>
-  );
+const ClickedText = ({text}) => {
+    return (
+        <div className="text-sm">
+            <a
+                href="#"
+                className="font-instrument font-medium text-${props.color} hover:text-${props.hoverColor}"
+            >
+                {Text}
+            </a>
+        </div>
+    );
 };
 
 export default ClickedText;
