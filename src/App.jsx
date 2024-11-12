648ef35bc105b859c03620e5d91f6296891d8e53
Gilang Indra <gilangindra734@gmail.com>
2024-11-13 05:49:27 +0700

    🔥 feat: add new route in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Login from "../src/pages/Login";
 import Dashboard from "../src/pages/mahasiswa/Dashboard";
 import LandingPage from "./pages/LandingPages";
@@ -7,24 +7,28 @@ import Status from "../src/pages/mahasiswa/Status";
 import NotFound from "../src/pages/NotFound";
 import DashboardLayoutMahasiswa from "./components/DashboardLayoutMahasiswa";
 import Persyaratan from "./pages/mahasiswa/Persyaratan";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PascaSeminar from "./pages/mahasiswa/PascaSeminar.jsx";
+import DashboardInstansi from "./pages/instansi/Dashboard.jsx"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-        <Route element={<DashboardLayoutMahasiswa />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/pendaftaran" element={<Pendaftaran />} />
-          <Route path="/persyaratan" element={<Persyaratan />} />
-          <Route path="/status" element={<Status />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="*" element={<NotFound/>}/>
+                <Route element={<DashboardLayoutMahasiswa/>}>
+                    <Route path="/mahasiswa/dashboard" element={<Dashboard/>}/>
+                    <Route path="/mahasiswa/pendaftaran" element={<Pendaftaran/>}/>
+                    <Route path="/mahasiswa/persyaratan" element={<Persyaratan/>}/>
+                    <Route path="/mahasiswa/pascaSeminar" element={<PascaSeminar/>}/>
+                    <Route path="/mahasiswa/status" element={<Status/>}/>
+                </Route>
+                <Route path="/pembimbingInstansi/Dashboard" element={<DashboardInstansi/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
