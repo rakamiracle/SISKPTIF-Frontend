918b92f97ef8fb11024338c2d8d77df15ce65f66
Gilang Indra <gilangindra734@gmail.com>
2024-11-13 05:56:02 +0700

    🔥 feat : add pasca-seminar pages

---
+++ src/pages/mahasiswa/PascaSeminar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Upload from '../../components/Upload'
+
+const PascaSeminar = () => {
+    return (
+        <div>
+            Pasca Seminar
+        </div>
+    )
+}
+
+export default PascaSeminar
